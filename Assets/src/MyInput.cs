37eaeaa258422240a75a75a41d80afe983e1ca50
Kari Tuurihalme <ukkostaja@iki.fi>
2018-01-26 21:47:46 +0200

    scene vailita

--- Assets/src/MyInput.cs
+++ Assets/src/MyInput.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class MyInput : MonoBehaviour {
 	bool notagain;
 
+	public SceneID[] activeObjects;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,10 +16,29 @@ public class MyInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.E && )) {
+		if (Input.GetKey(KeyCode.E) && notagain == false) {
+			notagain = true;
+			GameObject me = this.gameObject;
+			SceneID winner = new SceneID(0);
+			float winnerdistance = float.MaxValue;
+
+			float distance;
+			foreach (SceneID sid in activeObjects) {
+				distance = Vector3.Distance(sid.gameObject.transform.position, this.gameObject.transform.position);
+				if (distance < winnerdistance) {
+					winnerdistance = distance;
+					winner = sid;	
+				}
+
+			}
 
 
-			SceneManager.LoadScene ("minigame2");
+
+
+			SceneManager.LoadScene(winner.id);
 		}
+
+
+
 	}
 }
