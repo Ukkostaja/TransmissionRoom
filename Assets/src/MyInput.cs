21923f9d5554f7117262e2a7e0f74740c2d7c927
Kari Tuurihalme <ukkostaja@iki.fi>
2018-01-26 21:14:35 +0200

    collision

--- Assets/src/MyInput.cs
+++ Assets/src/MyInput.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MyInput : MonoBehaviour {
+	bool notagain;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		bool notagain = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.E)) {
+		if (Input.GetKey(KeyCode.E && )) {
+
+
 			SceneManager.LoadScene ("minigame2");
 		}
 	}
