5a463734d4fe646f79396655f6b9b5c09bdb1b98
tuomas-clusterloop <tuomas.kolttola@clusterloop.fi>
2018-01-27 21:52:14 +0200

    radio toimii v2

--- Assets/src/Minigame4/bassstream.cs
+++ Assets/src/Minigame4/bassstream.cs
@@ -5,11 +5,21 @@ using System.Reflection;
 using System.Runtime;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using SimpleJSON;
+using Random = UnityEngine.Random;
 
 public class bassstream : MonoBehaviour {
 
-	public string url;
-	private int stream;
+//	public static string url;
+
+	public static JSONNode json;
+
+	private static string streamurl;
+
+	private static int stream;
+
+	private static int randomchannel;
+	private static float randomfloat;
 
 
 	public enum flags {
@@ -44,22 +54,38 @@ public class bassstream : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
+	public static void Play () {
+		
+		 
+		randomfloat = Random.Range(0, 19);
+		randomchannel = Mathf.RoundToInt (randomfloat);
+		streamurl = json [0] [randomchannel] [10] [0] [0];
+
 		if ( BASS_Init(-1, 44100,0,IntPtr.Zero, IntPtr.Zero) )
 		{
 			BASS_SetConfig(configs.BASS_CONFIG_NET_PLAYLIST,2);
-			stream = BASS_StreamCreateURL(url, 0, flags.BASS_DEFAULT, IntPtr.Zero, IntPtr.Zero);
+			stream = BASS_StreamCreateURL(streamurl, 0, flags.BASS_DEFAULT, IntPtr.Zero, IntPtr.Zero);
 
 			if (stream != 0)
-				BASS_ChannelPlay(stream, false);          
+				BASS_ChannelPlay (stream, false);
+			else {
+				Debug.Log ("stream = 0");
+				Debug.Log ("URL = " + streamurl);
+			}
 		}
 	}
 	
 	// Update is called once per frame
+	//TEMPORARY
+	//MOVE
 	void Update () {
-		
+		if (Input.GetKey (KeyCode.E)) {
+			BASS_StreamFree (stream);
+			BASS_Free ();
+			Play ();
+		}
 	}
-
+	//stop the audio
 	void OnApplicationQuit()
 	{
 		BASS_StreamFree(stream);
---
+++ Assets/src/Minigame4/getUrl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+//using System;
+using System.IO;
+using SimpleJSON;
+//using Newtonsoft.Json.Linq
+//using LitJSON;
+
+public class getUrl : MonoBehaviour {
+
+	public float offsetfloat;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		//select the channels from random position
+		float offsetrandfloat = Random.Range(0, offsetfloat);
+		int offsetrand = Mathf.RoundToInt (offsetrandfloat);
+		string offset = "offset=" + offsetrand;
+
+		//get 20 channels
+		UnityWebRequest www = UnityWebRequest.Get ("http://api.dirble.com/v2/stations?" + offset + "&token=8a3834b981c1bbad181e0ad725");
+
+		yield return www.SendWebRequest();
+
+		if(www.isNetworkError || www.isHttpError) {
+			Debug.Log(www.error);
+		}
+		else {
+			// Show results as text
+//			Debug.Log(www.downloadHandler.text);
+
+			// Or retrieve results as binary data
+			byte[] results = www.downloadHandler.data;
+		}
+
+		//set 20 channels to jsonnode object
+		string jsonstring = www.downloadHandler.text;
+		string jsonstring2 = "{\"Items\":" + jsonstring + "}";
+		JSONNode jsonobj = JSONNode.Parse(jsonstring2);
+
+		//send found channels to player
+		bassstream.json = jsonobj;
+		bassstream.Play ();
+
+		//play the first channel
+		//bassstream.Play(jsonobj[0][0][10][0][0]);
+
+		//play second channel
+		//Debug.Log (jsonobj[0][1][10][0][0]);
+
+		//etc
+		//Debug.Log (jsonobj[0][2][10][0][0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
