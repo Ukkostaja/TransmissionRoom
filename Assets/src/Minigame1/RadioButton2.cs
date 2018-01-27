d503e625b771338f500783c6c2a3fc4b430c4661
Kari Tuurihalme <ukkostaja@iki.fi>
2018-01-27 14:56:11 +0200

    RadioButton2.cs eka jotenkin toimiva versio

---
+++ Assets/src/Minigame1/RadioButton2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioButton2 : MonoBehaviour,Interactable {
+
+
+	RadioVisualTuning RVT;
+	// Use this for initialization
+	void Start () {
+		RVT = GetComponentInParent<RadioVisualTuning> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Mouse1() {
+		RVT.Left ();
+	}
+
+	public void Mouse2() {
+		RVT.Right ();
+	}
+
+}
---
+++ Assets/src/Minigame1/RadioVisualTuning.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioVisualTuning : MonoBehaviour {
+
+	public GameObject RadioNeedle;
+	public GameObject LeftBound;
+	public GameObject RightBound;
+	public float StepLegnth = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		if(RadioNeedle == null || LeftBound == null || RightBound == null) Debug.Log("Not all GameObjects set in Editor");
+	}
+
+	public void Left() {
+		if (RadioNeedle.transform.localPosition.z - StepLegnth < LeftBound.transform.localPosition.z) {
+			Vector3 newPos= RadioNeedle.transform.localPosition;
+			newPos.z -= StepLegnth;
+			RadioNeedle.transform.localPosition = newPos;
+			Tune ();
+		}
+
+
+
+	}
+
+
+	public void Tune (){
+
+	}
+
+
+	public void Right() {
+		if (RadioNeedle.transform.localPosition.z + StepLegnth > RightBound.transform.localPosition.z) {
+			Vector3 newPos= RadioNeedle.transform.localPosition;
+			newPos.z += StepLegnth;
+			RadioNeedle.transform.localPosition = newPos;
+			Tune ();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
