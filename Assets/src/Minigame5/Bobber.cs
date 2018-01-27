ee986898c76f76f193a3b5757d15383afe2721a3
Kari Tuurihalme <ukkostaja@iki.fi>
2018-01-27 21:55:58 +0200

    Kalastus kenttä

---
+++ Assets/src/Minigame5/Bobber.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bobber : MonoBehaviour {
+
+	public float speed;
+	public Vector3 target;
+	Vector3 pore;
+	Vector3 liikevektori;
+	public bool active;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active) {
+
+		}
+	}
+
+
+	void Gogo(Vector3 pos) {
+		liikevektori = (pos-gameObject.transform.position)/1000;
+	}
+
+	public void setPOR(Vector3 POS) {
+		pore = POS;
+	}
+
+	void Return() {
+		Gogo (pore);
+	}
+}
---
+++ Assets/src/Minigame5/Kalasta.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kalasta : MonoBehaviour {
+	Animator anim;
+	public GameObject pointOfReturn;
+	Bobber koho;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		koho = GetComponentInChildren<Bobber> ();
+		koho.transform.position = pointOfReturn.transform.position;
+		koho.setPOR (pointOfReturn.transform.position);
+	}
+
+
+	public void Fish() {
+		anim.CrossFade("New Animation",0.1f);
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Fish ();
+
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			Physics.Raycast (ray, out hit);
+			Vector3 targetPoint = hit.point;
+			if (targetPoint != Vector3.zero) {
+				if (targetPoint.z > 1) {
+					koho.gogo (targetPoint);
+				}
+			}
+
+			Debug.Log (targetPoint);
+		}
+
+	}
+}
