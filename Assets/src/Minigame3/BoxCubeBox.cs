556c3c059787e82a15432534bdd768ff71638cf2
Juha Tapio Koivistoinen <juha.t.koivistoinen@jyu.fi>
2018-01-27 14:13:53 +0200

    minigame3 scripts

---
+++ Assets/src/Minigame3/BoxCubeBox.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCubeBox : MonoBehaviour {
+
+	Vector3 cubePosition;
+	Vector3 mousePosition;
+	Vector3 offsetVector;
+
+	float offset = 10f;
+	public float dragheight = 5.5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cubePosition = transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		offsetVector = mousePosition - cubePosition;
+	}
+
+	void OnMouseDown()
+	{
+		if (Mathf.Abs(offsetVector.x) < offset && Mathf.Abs(offsetVector.x) < offset && Mathf.Abs(offsetVector.x) < offset)
+		{
+			cubePosition = mousePosition;
+			transform.position = new Vector3(cubePosition.x, dragheight, cubePosition.z);
+		}
+	}
+
+	void OnMouseDrag()
+	{
+		cubePosition = mousePosition;
+		transform.position = new Vector3 (cubePosition.x, dragheight, cubePosition.z);
+	}
+
+	void OnMouseUp()
+	{
+		transform.position = new Vector3 (cubePosition.x, dragheight, cubePosition.z);
+	}
+}
---
+++ Assets/src/Minigame3/BoxTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTrigger : MonoBehaviour {
+
+	int objectsTriggered = 0;
+
+	// This property is utilized by the scene in order to see if the puzzle has been resolved.
+	bool threeObjectsTriggered = false;
+	public bool ThreeObjectsTriggered{
+		get { return threeObjectsTriggered; }
+		set { threeObjectsTriggered = value; }
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "CubeBoxCube") objectsTriggered++;
+		IsThreeObjectsTriggered ();
+	}
+
+	bool IsThreeObjectsTriggered(){
+		if (objectsTriggered > 2)
+			return true;
+		else
+			return false;
+	}
+}
