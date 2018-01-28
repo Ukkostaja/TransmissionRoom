754507ccb824a1d7da6c94dae077259ef2a3aed7
tuomas-clusterloop <tuomas.kolttola@clusterloop.fi>
2018-01-28 12:28:56 +0200

    minigamet linkitetty mainiin alustavasti

--- Assets/src/PointAndClick.cs
+++ Assets/src/PointAndClick.cs
@@ -9,8 +9,7 @@ public class PointAndClick : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Cursor.visible = true;
-		Cursor.lockState = CursorLockMode.None;
+
 	}
 	
 	// Update is called once per frame
